d3c8b809c1ebb2457f12bd51a917d035efa7d6b9
pavleli <pavleli@tencent.com>
2021-09-03 11:18:40 +0800

    init

--- pkg/provider.go
+++ pkg/provider.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"github.com/pavlelee/vscode-test-module/pkg/provider"
+	"github.com/pavlelee/vscode-test-module/pkg/registry"
 )
 
 type Provider struct {
@@ -10,6 +11,7 @@ type Provider struct {
 var _ provider.Provider = &Provider{}
 
 func (p *Provider) Name() string {
+	registry.New(nil)
 	panic("not implemented") // TODO: Implement
 }
 
